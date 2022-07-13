be274465450c46bb7e178f40178ea035585201c8
Emir Topaloğlu <emirrttopaloglu@gmail.com>
2022-07-13 18:28:51 +0300

    Search component updated

--- src/components/Search.js
+++ src/components/Search.js
@@ -1,8 +1,44 @@
-import React from 'react'
+import { useState } from 'react';
+import styled from 'styled-components'
+
+const InputWrapper = styled.div`
+    display: flex;
+    justify-content: space-between;
+    border: 2px solid #4a90e2;
+    border-radius: 4px;
+`;
+
+const Input = styled.input`
+    width: 100%;
+    padding: 12px 15px;
+    font-size: 13px;
+    font-weight: 600;
+    outline: none;
+    border: none;
+`;
+
+const Button = styled.button`
+    background: #4a90e2;
+    border: none;
+    position: relative;
+    right: -1px;
+    width: 125px;
+    color: #fff;
+    font-weight: 700;
+`;
 
 function Search() {
+  const [search, setSearch] = useState('');
   return (
-    <div>Search</div>
+    <InputWrapper>
+        <Input 
+            type="text"
+            value={search}
+            onChange={(e) => setSearch(e.target.value)}
+            placeholder="Binlerce mağaza arasından en ucuz fiyatı bulun!" />
+        
+        <Button>ARA</Button>
+    </InputWrapper>
   )
 }
 
